c4d1ac0c62bee82017c3d8a37d8858bfa224589f
rakibulislam01 <mrakibul70@gmail.com>
2019-04-19 22:49:19 +0600

    Add update and delete

--- mainBlog/blogApp/api/urls.py
+++ mainBlog/blogApp/api/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
-from . import views
+from .views import (PostListAPIView,
+                    PostUpdateAPIView,
+                    PostDeleteAPIView,
+                    PostDetailAPIView)
 
-# This is For check
 urlpatterns = [
-    url('list/', views.PostListAPIView.as_view(), name="list"),
-    url(r'^(?P<pk>\d+)/$', views.PostDetailAPIView.as_view(), name="detail"),
-    # url('postlist/', views.post_list, name="list"),
-    # url(r'^(?P<id>\d+)/edit/$', views.post_update, name="update"),
-    # url(r'^(?P<id>\d+)/delete/$', views.post_delete),
-    # # path('jango', views.index, name='jango'),
+    url(r'^$', PostListAPIView.as_view(), name="list"),
+    url(r'^(?P<pk>\d+)/$', PostDetailAPIView.as_view(), name="detail"),
+    url(r'^(?P<pk>\d+)/edit/$', PostUpdateAPIView.as_view(), name="update"),
+    url(r'^(?P<pk>\d+)/delete/$', PostDeleteAPIView.as_view(), name="delete"),
 ]
