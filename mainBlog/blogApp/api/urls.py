082966d0de1b13851efa10ec500fe2d569608b18
rakibulislam01 <mrakibul70@gmail.com>
2019-04-20 14:04:56 +0600

    Filtering a Queryset in a ListAPIView

--- mainBlog/blogApp/api/urls.py
+++ mainBlog/blogApp/api/urls.py
@@ -4,11 +4,14 @@ from .views import (PostListAPIView,
                     PostUpdateAPIView,
                     PostDeleteAPIView,
                     PostDetailAPIView)
+from django.views.decorators.csrf import csrf_exempt
+
 
 urlpatterns = [
     url(r'^$', PostListAPIView.as_view(), name="list"),
     url(r'^create/$', PostCreateAPIView.as_view(), name="create"),
     url(r'^(?P<pk>\d+)/$', PostDetailAPIView.as_view(), name="detail"),
+    # url(r'^api/v1/some-resource$', csrf_exempt(SomeApiView.as_view())),
     url(r'^(?P<pk>\d+)/edit/$', PostUpdateAPIView.as_view(), name="update"),
     url(r'^(?P<pk>\d+)/delete/$', PostDeleteAPIView.as_view(), name="delete"),
 ]
--- mainBlog/blogApp/api/views.py
+++ mainBlog/blogApp/api/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.generics import (ListAPIView,
                                      CreateAPIView,
                                      UpdateAPIView,
@@ -33,12 +35,14 @@ class PostUpdateAPIView(RetrieveUpdateAPIView):
     queryset = Post.objects.all()
     serializer_class = PosCreateUpdateSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerReadonly]
+
     # lookup_field = 'slug'
     # lookup_url_kwarg = 'abc'
 
     def perform_update(self, serializer):
-        instance = serializer.save()
-        # send_email_confirmation(user=self.request.user, modified=instance)
+        serializer.save(user=self.request.user)
+    #     instance = serializer.save()
+    #     # send_email_confirmation(user=self.request.user, modified=instance)
 
 
 class PostDeleteAPIView(DestroyAPIView):
@@ -49,5 +53,19 @@ class PostDeleteAPIView(DestroyAPIView):
 
 
 class PostListAPIView(ListAPIView):
-    queryset = Post.objects.all()
     serializer_class = PostListSerializer
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title', 'content', 'user__first_name']
+
+    def get_queryset(self, *args, **kwargs):
+        queryset_list = Post.objects.all()
+        # queryset_list = super(PostListAPIView, super).get_queryset(*args, **kwargs)
+        query = self.request.GET.get("q")
+        if query:
+            queryset_list = queryset_list.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(user__first_name__icontains=query) |
+                Q(user__last_name__icontains=query)
+                ).distinct()
+        return queryset_list
